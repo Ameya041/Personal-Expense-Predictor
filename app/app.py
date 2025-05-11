88b527b59fb4152abaa38cfd1509c1dde630de11
Amey Hiremath <155788608+Ameya041@users.noreply.github.com>
2025-05-11 22:44:51 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -43,4 +43,5 @@ def index():
     return render_template('form.html', prediction=prediction, chart_data=chart_data)
 
 if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
     app.run(debug=os.getenv('FLASK_DEBUG', 'False') == 'True')
